7dbba6a4cf5b57b2d8c3e4891c16f62c29465348
Nicolas Senave <nicolas.senave@gmail.com>
2024-02-13 16:10:07 +0100

    test(suggester): deserialization test for query params

--- src/test/java/fr/insee/pogues/test/suggester/SuggesterSerializationTests.java
+++ src/test/java/fr/insee/pogues/test/suggester/SuggesterSerializationTests.java
@@ -1,5 +1,6 @@
 package fr.insee.pogues.test.suggester;
 
+import fr.insee.pogues.conversion.JSONDeserializer;
 import fr.insee.pogues.conversion.JSONSerializer;
 import fr.insee.pogues.model.*;
 import org.json.JSONException;
@@ -8,20 +9,23 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import javax.xml.bind.JAXBException;
+import java.io.ByteArrayInputStream;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SuggesterSerializationTests {
 
     @Test
     void serializeSuggesterQueryParserParams() throws JAXBException, UnsupportedEncodingException, JSONException {
-        //
+        // Given a suggester query params
         SuggesterQueryParserParams queryParserParams = new SuggesterQueryParserParams();
         queryParserParams.setLanguage("FR");
         queryParserParams.setMin(BigInteger.ONE);
         queryParserParams.setPattern("(\\w+)");
         queryParserParams.setStemmer(true);
-        //
+        // in a questionnaire object
         Questionnaire questionnaire = new Questionnaire();
         CodeList codeList = new CodeList();
         SuggesterQueryParser queryParser = new SuggesterQueryParser();
@@ -31,11 +35,11 @@ class SuggesterSerializationTests {
         questionnaire.setCodeLists(new CodeLists());
         questionnaire.getCodeLists().getCodeList().add(codeList);
 
-        //
+        // When
         JSONSerializer jsonSerializer = new JSONSerializer();
         String result = jsonSerializer.serialize(questionnaire);
 
-        //
+        // Then
         String expectedJson = """
                 {
                   "Questionnaire": {
@@ -58,6 +62,44 @@ class SuggesterSerializationTests {
                   }
                 }""";
         JSONAssert.assertEquals(expectedJson, result, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    void deserializeSuggesterQueryParserParams() throws JAXBException {
+        // Given
+        String jsonInput = """
+                {
+                  "CodeLists": {
+                    "CodeList": [
+                      {
+                        "SuggesterParameters": {
+                          "queryParser": {
+                            "params": {
+                              "language": "FR",
+                              "min": 1,
+                              "pattern": "(\\\\w+)",
+                              "stemmer": true
+                            }
+                          }
+                        }
+                      }
+                    ]
+                  }
+                }""";
+
+        // When
+        JSONDeserializer jsonDeserializer = new JSONDeserializer();
+        Questionnaire questionnaire = jsonDeserializer.deserialize(new ByteArrayInputStream(jsonInput.getBytes()));
+
+        // Then
+        // (assuming intermediate objects are fine)
+        SuggesterQueryParserParams queryParserParams = questionnaire.getCodeLists().getCodeList().getFirst()
+                .getSuggesterParameters().getQueryParser().getParams();
+        //
+        assertEquals("FR", queryParserParams.getLanguage());
+        assertEquals(BigInteger.ONE, queryParserParams.getMin());
+        assertEquals("(\\w+)", queryParserParams.getPattern());
+        assertEquals(true, queryParserParams.isStemmer());
     }
 
 }
